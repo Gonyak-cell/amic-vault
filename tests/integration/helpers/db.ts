ed2b5837813d77e9377427c21043d4b9a70fb0c0
MAC <jws@MACui-MacBookPro.local>
2026-07-22 10:28:50 +0900

    test(db): DEVOPS-OSS01-DBA-TUW-004 isolate runtime role

--- tests/integration/helpers/db.ts
+++ tests/integration/helpers/db.ts
@@ -1,10 +1,12 @@
 import { Client } from 'pg';
 
 const databaseUrl =
+  process.env.DATABASE_MIGRATION_URL ??
   process.env.DATABASE_URL ??
   'postgres://amic_vault:amic_vault_dev_password@localhost:5432/amic_vault';
 
 const appDatabaseUrl =
+  process.env.DATABASE_RUNTIME_URL ??
   process.env.APP_DATABASE_URL ??
   'postgres://vault_app:vault_app_dev_password@localhost:5432/amic_vault';
 
